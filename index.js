02bed022dd453de70624212f2f59ac37d7e7fef7
nguyenkaos <nguyen.caal@gmail.com>
2017-12-30 18:24:57 +0700

    ok

---
+++ index.js
@@ -0,0 +1,52 @@
+const http = require('http');  
+
+ 
+ 
+(async () => { 
+  const requestHandler = (request, response) => {  
+       console.log(request.url)
+       response.end('Hello World')
+  }
+       
+  const server = http.createServer(requestHandler)
+
+  server.listen(3000, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
+ 
+})();
+
+
+const puppeteer = require('puppeteer');
+function kiusi(){
+  (async () => {
+  console.log('-----kiusi00000-----------');
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  
+  const page = await browser.newPage();
+  await page.goto('https://cnhv.co/yhgz'); 
+  console.log('----XONG0--------');
+  setTimeout(async () => await await page.close(), 67000);
+  setTimeout(async () => await browser.close(), 75000);
+  console.log('----XONG--------');
+  
+  })();
+}
+
+
+
+function f() { 
+    console.log('-111111111111-------trong f -----------');
+    kiusi();
+    console.log(Date()); 
+    setTimeout( f, 120000 ); 
+    console.log('--tttttttttt--') 
+}
+
+
+f();
+ 
