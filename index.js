9b7880fa338f7c5356cf92fe6510650e86b0d8bd
toilaaitrong <43970266+toilaaitrong@users.noreply.github.com>
2018-10-09 02:34:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function kiusi(){
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   
   const page = await browser.newPage();
-  await page.goto('https://toilaaitrong.github.io/lucatoni/index.html'); 
+  await page.goto('https://toilaaitrong.github.io/ludqzedf/index.html'); 
   console.log('----XONG0--------');
   setTimeout(async () => await await page.close(), 185000);
   setTimeout(async () => await browser.close(), 193000);
